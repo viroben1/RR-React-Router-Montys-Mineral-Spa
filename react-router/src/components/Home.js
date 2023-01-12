206f883171e801286e576ca810acaf99f31c5f50
benjamin farmer <viroben2001@gmail.com>
2023-01-11 19:26:53 -0800

    added card

--- react-router/src/components/Home.js
+++ react-router/src/components/Home.js
@@ -1,13 +1,15 @@
-
-
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
 export default function Home () {
     return(
         <div className="homepage">
-            <h1>Visit our resort and change your life!</h1>
-
-            <h1>Grand opening in StressVille, Texas</h1>
-            
-            <h1>Check out our wide array of packages and offers!</h1>
+            <Container>
+                <Card>
+                <h1>Visit our resort and change your life!</h1>
+                <h1>Grand opening in StressVille, Texas</h1>
+                <h1>Check out our wide array of packages and offers!</h1>
+                </Card>
+                </Container>
 
         </div>
     )
